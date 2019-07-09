63880a04aa160e7714f405c3da70f58144558ffe
Julien Palard <julien@palard.fr>
2019-07-09 11:25:08 +0200

    Accept certificates from a file.

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-Prints (on stderr) the list and expiration time ssl certificates of
-all given domains like:
+List sexpiration time of soon-to-be expired ssl certificates of all
+given domains like:
 
    ./certificate_watcher.py mdk.fr python.org duckduckgo.com
    mdk.fr expire in 2 days
 
-Usefull as a daily cron, mainly.
+The list of domains can be given as a file, using the `-f` or
+`--from-file` argument. In this file, list each domains, one per
+line. Black lines and lines starting with '#' are ignored.
+
+Exemple domain file:
+
+```
+# Our project
+example.com
+www.example.com
+mail.example.com
+dev.example.com
+demo.example.com
+testing.example.com
+git.example.com
+
+# Friends
+duckduckgo.com
+mamot.fr
+protonmail.com
+```
+
+Usefull as a daily cron, typically like this:
+
+   certificate_watcher -f /home/you/.certificate_watcher
+
+Or if you use IRC:
+
+   certificate_watcher -f /home/you/.certificate_watcher | irk '#your_project_channel' -
--- certificate_watcher.py
+++ certificate_watcher.py
@@ -8,10 +8,11 @@ all given domains like:
 
 """
 
-import sys
-import ssl
-import socket
+import argparse
 from datetime import datetime, timedelta
+import socket
+import ssl
+import sys
 
 
 __version__ = "0.0.1"
@@ -26,9 +27,34 @@ def get_server_certificate(addr, port=443, timeout=10):
             return sslsock.getpeercert()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="Certificate Watcher",
+        description="Watch expiration of certificates of a bunch of websites.",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument(
+        "-f",
+        "--from-file",
+        type=argparse.FileType("r"),
+        help="Check host from this file (one per line)",
+    )
+    parser.add_argument("hosts", nargs="*", help="Hosts to check")
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s " + __version__
+    )
+    return parser.parse_args()
+
+
 def main():
-    verbose = '-v' in sys.argv
-    hosts = [host for host in sys.argv[1:] if host != '-v']
+    args = parse_args()
+    hosts = args.hosts
+    if args.from_file:
+        hosts.extend(
+            host.strip()
+            for host in args.from_file.read().split("\n")
+            if host and not host.startswith("#")
+        )
     now = datetime.utcnow()
     limit = timedelta(days=14)
     for host in hosts:
@@ -38,17 +64,23 @@ def main():
             print("{host}: connect timeout".format(host=host))
         except ConnectionResetError:
             print("{host}: Connection reset".format(host=host))
-        except (ssl.CertificateError, socket.gaierror, ssl.SSLError, ConnectionRefusedError) as err:
+        except (
+            ssl.CertificateError,
+            socket.gaierror,
+            ssl.SSLError,
+            ConnectionRefusedError,
+        ) as err:
             print("{host}: {err}".format(host=host, err=str(err)))
         else:
-            not_after = datetime.strptime(cert['notAfter'],
-                                        "%b %d %H:%M:%S %Y GMT")
+            not_after = datetime.strptime(cert["notAfter"], "%b %d %H:%M:%S %Y GMT")
             expire_in = not_after - now
-            if expire_in < limit or verbose:
-                print("{host} expire in {expire_in:.0f} days".format(
-                    host=host,
-                    expire_in=expire_in.total_seconds() // 86400))
+            if expire_in < limit or args.verbose:
+                print(
+                    "{host} expire in {expire_in:.0f} days".format(
+                        host=host, expire_in=expire_in.total_seconds() // 86400
+                    )
+                )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
